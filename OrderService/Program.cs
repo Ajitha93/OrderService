f2e87af582659d9a5402d9225ace6ea395f6bc49
Windows <Windows@DESKTOP-UKKVFVK>
2024-10-11 23:01:42 -0500

    removed swagger

--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -28,8 +28,8 @@ builder.Services.AddControllers()
      });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<RestaurantContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
