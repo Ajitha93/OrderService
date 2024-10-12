28a3017003a9aeab0ee7b0d84305e1d50d9eaa49
Windows <Windows@DESKTOP-UKKVFVK>
2024-10-11 22:57:18 -0500

    Dev env removed

--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -37,11 +37,11 @@ builder.Services.AddDbContext<RestaurantContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
