63a9a5509f7be7828723ef2ca268e894774586b0
ajitha gangineni <gangineni.ajitha@gmail.com>
2024-12-14 14:10:17 -0600

    ManagedIdentity Changes

--- OrderUpsertService/Program.cs
+++ OrderUpsertService/Program.cs
@@ -1,5 +1,6 @@
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using OrderDBContext;
@@ -31,16 +32,34 @@ builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Retrieve connection string from Key Vault
-var keyVaultUrl = "https://orderservicekeyvalut.vault.azure.net/"; // Your Key Vault URL
-var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
-KeyVaultSecret secret = client.GetSecret("orderconnectionstring");
-var connectionString = secret.Value;
+//// Retrieve connection string from Key Vault
+//var keyVaultUrl = "https://orderservicekeyvalut.vault.azure.net/"; // Your Key Vault URL
+//var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+//KeyVaultSecret secret = client.GetSecret("orderconnectionstring");
+//var connectionString = secret.Value;
+
+//builder.Services.AddDbContext<RestaurantContext>(options =>
+//               options.UseSqlServer(connectionString));
+
+// Managed Identity start
+//string sqlConnectionString = "Server=tcp:ordermgtsqlserver1.database.windows.net,1433;Database=orderscan";
+
+string sqlConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Use DefaultAzureCredential to authenticate via Managed Identity
+var credential = new DefaultAzureCredential();
+var token = await credential.GetTokenAsync(new Azure.Core.TokenRequestContext(new[] { "https://database.windows.net/.default" }));
+
+// Use the token to authenticate to Azure SQL Database
+var connection = new SqlConnection(sqlConnectionString);
+connection.AccessToken = token.Token;
+
+await connection.OpenAsync();
 
 builder.Services.AddDbContext<RestaurantContext>(options =>
-               options.UseSqlServer(connectionString));
-//builder.Services.AddDbContext<RestaurantContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(connection));
+// Managed Identity end
+
 
 var app = builder.Build();
 
