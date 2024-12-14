0e5e1669bc4b4dec428bad4cb294c0a1514ba130
ajitha gangineni <gangineni.ajitha@gmail.com>
2024-12-14 12:03:01 -0600

    Logging included

--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -43,25 +43,33 @@ builder.Services.AddControllers()
 
 //builder.Services.AddDbContext<RestaurantContext>(options =>
 //               options.UseSqlServer(connectionString));
+try
+{
+    // Managed Identity start
+    //string sqlConnectionString = "Server=tcp:ordermgtsqlserver1.database.windows.net,1433;Database=orderscan";
 
-// Managed Identity start
-//string sqlConnectionString = "Server=tcp:ordermgtsqlserver1.database.windows.net,1433;Database=orderscan";
+    string sqlConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-string sqlConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    // Use DefaultAzureCredential to authenticate via Managed Identity
+    var credential = new DefaultAzureCredential();
+    var token = await credential.GetTokenAsync(new Azure.Core.TokenRequestContext(new[] { "https://database.windows.net/.default" }));
 
-// Use DefaultAzureCredential to authenticate via Managed Identity
-var credential = new DefaultAzureCredential();
-var token = await credential.GetTokenAsync(new Azure.Core.TokenRequestContext(new[] { "https://database.windows.net/.default" }));
+    // Use the token to authenticate to Azure SQL Database
+    var connection = new SqlConnection(sqlConnectionString);
+    connection.AccessToken = token.Token;
 
-// Use the token to authenticate to Azure SQL Database
-var connection = new SqlConnection(sqlConnectionString);
-connection.AccessToken = token.Token;
+    await connection.OpenAsync();
 
-await connection.OpenAsync();
-
-builder.Services.AddDbContext<RestaurantContext>(options =>
-    options.UseSqlServer(connection));
-// Managed Identity end
+    builder.Services.AddDbContext<RestaurantContext>(options =>
+        options.UseSqlServer(connection));
+    // Managed Identity end
+}
+catch (Exception ex)
+{
+    // Log the exception using ILogger
+    var logger = LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger("Application");
+    logger.LogError(ex, "An error occurred while connecting to the database.");
+}
 
 //builder.Services.AddDbContext<RestaurantContext>(options =>
 //    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
